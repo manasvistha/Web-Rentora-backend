aec05fd7f34083164264ccfb1745b9ce6de9a497
Mansvi Shrestha <manasvishrestha6@gmail.com>
2026-01-05 19:42:42 +0545

    middlewears

---
+++ src/middlewears/authorized.middlewears.ts
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config';
+
+export const authorize = (req: any, res: any, next: any) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'No token' });
+
+  req.user = jwt.verify(token, config.jwtSecret);
+  next();
+};
